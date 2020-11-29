bfb40f8cda005f4b12fef223e6e350aa85435a81
Ryan Kirkland <ryankirkland.py@gmail.com>
2020-11-28 21:02:10 -0700

    figure out multi-page layout reqs for plotly dash

--- app.py
+++ app.py
@@ -1,19 +1,113 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+import flask
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 
-app.layout = html.Div([
-    html.H6('Change the value in the box to see callbacks in action!'),
-    html.Div(["Input: ",
-            dcc.Input(id='my-input', value='initial value', type='text')]),
-    html.Br(),
-    html.Div(id='my-output'),
+url_bar_and_content_div = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
 ])
+
+layout_index = dbc.Container([
+        dbc.Navbar([
+            dbc.Row(
+                    dbc.Col([
+                        dcc.Link('Navigate to "/page-1"', href='/page-1'),
+                        dcc.Link('Navigate to "/page-2"', href='/page-2')
+                    ]),
+                    align="center",
+                    no_gutters=True,
+                ),
+                dbc.NavbarToggler(id="navbar-toggler")
+            ],
+            color="dark",
+            dark=True,
+        )
+    ])
+
+layout_page_1 = html.Div([
+    dbc.Navbar([
+            dbc.Row(
+                    dbc.Col([
+                        dcc.Link('Navigate to "/page-1"', href='/page-1'),
+                        dcc.Link('Navigate to "/page-2"', href='/page-2')
+                    ]),
+                    align="center",
+                    no_gutters=True,
+                ),
+                dbc.NavbarToggler(id="navbar-toggler")
+            ],
+            color="dark",
+            dark=True,
+        ),
+    html.H2('Page 1'),
+    dcc.Input(id='input-1-state', type='text', value='Montreal'),
+    dcc.Input(id='input-2-state', type='text', value='Canada'),
+    html.Button(id='submit-button', n_clicks=0, children='Submit'),
+    html.Div(id='output-state')
+])
+
+layout_page_2 = html.Div([
+    html.H2('Page 2'),
+    dcc.Dropdown(
+        id='page-2-dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='page-2-display-value'),
+    html.Br(),
+    dcc.Link('Navigate to "/"', href='/'),
+    html.Br(),
+    dcc.Link('Navigate to "/page-1"', href='/page-1'),
+])
+
+# index layout
+app.layout = url_bar_and_content_div
+
+# "complete" layout
+app.validation_layout = html.Div([
+    url_bar_and_content_div,
+    layout_index,
+    layout_page_1,
+    layout_page_2,
+])
+
+
+# Index callbacks
+@app.callback(Output('page-content', 'children'),
+              Input('url', 'pathname'))
+def display_page(pathname):
+    if pathname == "/page-1":
+        return layout_page_1
+    elif pathname == "/page-2":
+        return layout_page_2
+    else:
+        return layout_index
+
+
+# Page 1 callbacks
+@app.callback(Output('output-state', 'children'),
+              Input('submit-button', 'n_clicks'),
+              State('input-1-state', 'value'),
+              State('input-2-state', 'value'))
+def update_output(n_clicks, input1, input2):
+    return ('The Button has been pressed {} times,'
+            'Input 1 is "{}",'
+            'and Input 2 is "{}"').format(n_clicks, input1, input2)
+
+
+# Page 2 callbacks
+@app.callback(Output('page-2-display-value', 'children'),
+              Input('page-2-dropdown', 'value'))
+def display_value(value):
+    print('display_value')
+    return 'You have selected "{}"'.format(value)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
